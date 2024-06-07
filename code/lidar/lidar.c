8421750c393c73ef4280f7e1319d1846779caf5c
rmingon <ronanmingon69550@gmail.com>
2024-06-07 09:02:57 +0200

    delete code and add stm32f103 code

--- code/lidar/lidar.c
+++
@@ -1,8 +0,0 @@
-#ifndef LIDAR_DEFINES_H
-#define LIDAR_DEFINES_H
-
-void lidarInit(void);
-void lidarStop(void);
-void lidarSpeedMotor(uint16_t);
-
-#endif
--- code/linkit/linkit.c
+++
@@ -1,7 +0,0 @@
-#ifndef LINKIT_DEFINES_H
-#define LINKIT_DEFINES_H
-
-void linkitInit(void);
-void UART5_Init(void);
-
-#endif
