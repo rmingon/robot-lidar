58e7d27eedae1662da6a03eb0b69cc049bcaca8c
rmingon <ronanmingon69550@gmail.com>
2024-07-10 16:52:51 +0200

    test pin pwm

--- code/src/index.js
+++ code/src/index.js
@@ -1,8 +1,19 @@
 import { Motor } from './motor.js';
 
+
+const sleep = (microsecond) => {
+    return new Promise((resolve), () => {
+        setTimeout(resolve)
+    })
+}
 (async () => {
     const motor = new Motor()
     await motor.i2cInit()
     motor.readWord(0x00);
-    motor.setPwm(1, 4096, 0)
+    while(1) {
+        sleep(1000)
+        motor.setPwm(1, 0, 4095)
+        sleep(1000)
+        motor.setPwm(1, 4095, 0)
+    }
 }) ()
