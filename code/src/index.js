448e166f4afad177aa78a883a050e392236aa185
rmingon <ronanmingon69550@gmail.com>
2024-07-10 10:46:38 +0200

    add i2c

--- code/src/index.js
+++ code/src/index.js
@@ -1,2 +1,10 @@
-"use strict";
-console.log("HELLO");
+const i2c = require('i2c-bus');
+
+const MCP9685_ADDR = 0x40;
+const STATE_REG = 0x05;
+
+(async () => {
+    const i2cState = await i2c.openPromisified(1)
+    const rawData = await i2cState.readWord(MCP9685_ADDR, STATE_REG)
+    console.log(rawData);
+}) ()
