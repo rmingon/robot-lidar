9e97e203217824b909b812aec60f9a9a0d1734ee
rmingon <ronanmingon69550@gmail.com>
2024-07-10 16:55:23 +0200

    add await

--- code/src/index.js
+++ code/src/index.js
@@ -11,9 +11,9 @@ const sleep = (microsecond) => {
     await motor.i2cInit()
     motor.readWord(0x00);
     while(1) {
-        sleep(1000)
+        await sleep(1000)
         motor.setPwm(1, 0, 4095)
-        sleep(1000)
+        await sleep(1000)
         motor.setPwm(1, 4095, 0)
     }
 }) ()
